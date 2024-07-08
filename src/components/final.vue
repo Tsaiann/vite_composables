69909a320bba56e3048ed313cbb60a7f3ffa9f03
Tsaiann <ann110149@gmail.com>
2024-07-08 17:08:47 +0800

    add treeMenu / input / button

--- src/components/final.vue
+++ src/components/final.vue
@@ -1,7 +1,25 @@
 <template>
-  <div></div>
+  <div class="row vertical">
+    <div>
+      <el-input style="width: 250px;" />
+      <el-button type="info" plain @click="handleSearch()">搜尋</el-button>
+      <el-button type="danger" plain @click="onReset()">重置</el-button>
+    </div>
+    <div class="menu">
+      <treeMenu v-for="(i, item) in menuData" :key="item.key" v-model="menuData[item]"/>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { ref, reactive, onMounted } from 'vue'
+import treeMenu from './treeMenu.vue'
+import data from '../../data.json'
+
+const menuData = ref([])
+
+const getData = onMounted(()=>menuData.value = JSON.parse(JSON.stringify(data)))
+const handleSearch = () => {}
+const onReset = () => {}
 
 </script>
